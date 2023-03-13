e910a831cbe2f219651172912f17bd94e57925ac
Benedict Golpeo <alanbenedictgolpeo@gmail.com>
2023-03-13 23:55:19 +0800

    models and button

--- resources/views/widgets/branch_stocks.blade.php
+++ resources/views/widgets/branch_stocks.blade.php
@@ -1,8 +1,9 @@
-<div class="panel widget center bgimage" style="margin-bottom:0;overflow:hidden;background-image:url('/widget-bg/hardware.jpg');">
+<div class="panel widget center bgimage" style="margin-bottom:0;overflow:hidden;background-image:url('/widget-bg/warehouse.jpg');">
     <div class="dimmer"></div>
     <div class="panel-content">
-        <i class="voyager-shop"></i><h4>View other branch stocks</h4>
-        <p>Check products on other branches</p>
+        <i class="voyager-truck"></i><h4>Inbounds and Outbounds</h4>
+        <p>View stock arrivals and departures</p>
         <a href="http://192.168.0.103:88/admin/inventory" class="btn btn-primary">View</a>
+        {{-- <a href="http://192.168.0.103:88/admin/inventory" class="btn btn-primary">Create</a> --}}
     </div>
 </div>
