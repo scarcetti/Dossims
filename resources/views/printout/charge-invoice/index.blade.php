2da3697c1014389b9dfb0a550d5eb968a1891d20
scarcetti <chenvirtucio@gmail.com>
2023-03-31 10:14:13 +0800

    charge invoice

---
+++ resources/views/printout/charge-invoice/index.blade.php
@@ -0,0 +1,84 @@
+<style type="text/css">
+    table {
+        font-family: Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        /*     paper-si */
+        line-height: 25px;
+    }
+
+    div {
+        /*    border: solid;*/
+    }
+
+    .dflex {
+        display: grid;
+    }
+
+    .row {
+        flex-direction: row;
+    }
+
+    .column {
+        /*    float: left;*/
+        flex-direction: column;
+        position: relative;
+    }
+
+    .sec1 {
+        width: 30%;
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
+
+    .sec2 {
+        width: 68%;
+        position: absolute;
+        right: 0;
+        top: 0;
+    }
+
+    .sec3 {
+        width: 70%;
+        padding-left: 10px;
+        /*    background-color: aqua;*/
+        float: right;
+    }
+
+    .nt {
+        border-top: 0;
+    }
+
+    .nb {
+        border-bottom: 0;
+    }
+
+    .nob {
+        border: 0;
+    }
+
+    .ob {
+        border-left: 0;
+        border-top: 0;
+        border-right: 0;
+    }
+
+    th,
+    td {
+        border: solid 1px;
+        height: 28px;
+    }
+    .fr {
+        float: right
+    }
+
+    input[type="checkbox"] {}
+</style>
+<div style="position: relative; height: 230px;">
+    @include('printout.charge-invoice.section1')
+</div>
+<div style="position: relative; height: 400px;">
+    @include('printout.charge-invoice.section2')
+</div>
+
+
---
+++ resources/views/printout/charge-invoice/section1.blade.php
@@ -0,0 +1,44 @@
+{{-- @php
+    function toWord($x)
+    {
+        $f = new \NumberFormatter('en', NumberFormatter::SPELLOUT);
+        return strtoupper( $f->format($x) );
+    }
+@endphp --}}
+<div class="row" style="">
+    <span>
+        <img alt="asd" src="{{ public_path('images/domings.png') }}" style="width: 350px; margin-top: -20px">
+    </span>
+
+    <div style="position: absolute; right: 0; top: 0;">
+        <div style="font-size: 27px; font-weight: bold;">
+            DOMING'S STEEL TRADING
+        </div>
+        <div>
+            Prk. 5, McArthur Highway. Brgy Lizada, Toril, Davao City
+        </div>
+        <div>
+            Franz Marlon C. Quilban - Prop *VAT Reg. TIN: 416-676-701-002
+        </div>
+        <div>
+            Cell no. Smart 09209680226 / Globe 09752874632
+        </div>
+    </div>
+</div>
+<br>
+<div class="row" style=" position: relative;">
+    <div style="font-size: 25px; font-weight: bold;">
+        CHARGE INVOICE
+    </div>
+    <div style="position: absolute; right: 0; top: 5px;">
+        <table>
+            <tbody>
+                <tr>
+                    <td class="nob">Date</td>
+                    <td class="ob" width="80%" style="padding-left: 20px;">02 / 23 / 2023</td>
+                </tr>
+            </tbody>
+            {{-- Date ____________ --}}
+        </table>
+    </div>
+</div>
