aa6933a6c59597a07857aa55d5f549c9ee477f4d
scarcetti <chenvirtucio@gmail.com>
2023-05-17 23:21:12 +0800

    cutting list

---
+++ resources/views/printout/cutting-list/section1.blade.php
@@ -0,0 +1,26 @@
+<div class="row" style="">
+    <span>
+        <img alt="asd" src="{{ public_path('images/domings.png') }}" style="width: 350px; margin-top: -20px">
+    </span>
+
+    <div style="position: absolute; right: 0; top: 0;">
+        <div style="font-size: 27px; font-weight: bold;">
+            DOMING'S STEEL TRADING
+        </div>
+        <div>
+            Prk. 5, McArthur Highway. Brgy Lizada, Toril, Davao City
+        </div>
+        <div>
+            Franz Marlon C. Quilban - Prop *VAT Reg. TIN: 416-676-701-002
+        </div>
+        <div>
+            Cell no. Smart 09209680226 / Globe 09752874632
+        </div>
+    </div>
+</div>
+<br>
+<div class="row" style=" position: relative;">
+    <div style="font-size: 25px; font-weight: bold;">
+        CUTTING LIST
+    </div>
+</div>
