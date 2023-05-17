7cd1f847d440f8d05120652cb80d087ef5692c4c
scarcetti <chenvirtucio@gmail.com>
2023-05-17 23:17:05 +0800

    Job order

--- resources/views/printout/job-order/index.blade.php
+++ resources/views/printout/job-order/index.blade.php
@@ -1,3 +1,87 @@
 <div class="row">
 	@include('printout.job-order.section1')
 </div>
+<div class="row">
+	@include('printout.job-order.section2')
+</div>
+
+<style type="text/css">
+    table {
+        font-family: Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        /*     paper-si */
+        line-height: 25px;
+    }
+
+    div {
+        /*    border: solid;*/
+    }
+
+    .dflex {
+        display: grid;
+    }
+
+    .row {
+        flex-direction: row;
+    }
+
+    .column {
+        /*    float: left;*/
+        flex-direction: column;
+        position: relative;
+    }
+
+    .sec1 {
+        width: 30%;
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
+
+    .sec2 {
+        width: 68%;
+        position: absolute;
+        right: 0;
+        top: 0;
+    }
+
+    .sec3 {
+        width: 70%;
+        padding-left: 10px;
+        /*    background-color: aqua;*/
+        float: right;
+    }
+
+    .nt {
+        border-top: 0;
+    }
+
+    .nb {
+        border-bottom: 0;
+    }
+
+    .nob {
+        border: 0;
+    }
+
+    .ob {
+        border-left: 0;
+        border-top: 0;
+        border-right: 0;
+    }
+
+    th,
+    td {
+        border: solid 1px;
+        height: 28px;
+    }
+    .fr {
+        float: right
+    }
+    .basta {
+        border: 0;
+        text-align:right;
+    }
+
+    input[type="checkbox"] {}
+</style>
---
+++ resources/views/printout/job-order/section2.blade.php
@@ -0,0 +1,136 @@
+<div style=" position: relative; font-size:18px">
+    <table width="100%">
+        <tbody>
+            <tr>
+                <td class="nob" style="width: 20%">Name:</td>
+                <td class="ob" style="width: 25%" colspan="2">Elon Moist</td>
+                <td class="nob" style="width: 15%">Date</td>
+                <td class="ob" style="width: 25%">02 / 18 / 00</td>
+            </tr>
+            <tr>
+                <td class="nob">Address:</td>
+                <td class="ob"colspan="4">Unahan sa Tumoy</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<div style=" position: relative; font-size:18px; top: 20px;">
+    <table width="100%" >
+        <tbody>
+            <tr style="font-weight: bold;" align="center">
+                <td>QTY</td>
+                <td colspan="2">DESCRIPTION</td>
+                <td>U.P.</td>
+                <td>Amount</td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2"></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td class="basta" colspan="4">Total P</td>
+                <td class="basta" style="text-align: center">218</td>
+            </tr>
+            <tr>
+                <td class="basta" colspan="4">AMOUNT P</td>
+                <td class="basta" style="text-align: center">218</td>
+            </tr>
+            <tr>
+                <td class="basta" colspan="4">ADVANCE P</td>
+                <td class="basta" style="text-align: center">218</td>
+            </tr>
+            <tr>
+                <td class="basta" colspan="4">BALANCE P</td>
+                <td class="basta" style="text-align: center">218</td>
+            </tr>
+
+        </tbody>
+    </table>
+</div>
