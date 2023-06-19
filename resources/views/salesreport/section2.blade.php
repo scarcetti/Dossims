a339fa6dd8dc06ea89cb687dd077657a5b2758dc
scarcetti <chenvirtucio@gmail.com>
2023-06-19 22:26:32 +0800

    sales report templete

--- resources/views/salesreport/section2.blade.php
+++ resources/views/salesreport/section2.blade.php
@@ -5,7 +5,8 @@
                 <td colspan="3">
                     <p align="justify" style="padding: 10px;">
                         <b></b>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Sales report for the month of (user.input sa month) , (user.input sa year) for (user.input sa branch name) branch:
+                        Total Gross Sales: P<b>insert figure here</b>
                     </p>
                 </td>
             </tr>
