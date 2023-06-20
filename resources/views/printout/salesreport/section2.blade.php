e4fbf69cda7f696e677616f7ee692433c61cf1d1
Benedict Golpeo <alanbenedictgolpeo@gmail.com>
2023-06-21 02:45:22 +0800

    number format on sales report

--- resources/views/printout/salesreport/section2.blade.php
+++ resources/views/printout/salesreport/section2.blade.php
@@ -6,7 +6,7 @@
                     <p align="justify" style="padding: 10px;">
                         <b></b>
                         Sales report for the month of {{ $month }} , {{ $year }} for {{ $branch_name }} branch:
-                        Total Gross Sales: ₱<b>{{ $sum }}</b>
+                        Total Gross Sales: ₱&nbsp;<b>{{ number_format($sum, 2, '.', ',') }}</b>
                     </p>
                 </td>
             </tr>
