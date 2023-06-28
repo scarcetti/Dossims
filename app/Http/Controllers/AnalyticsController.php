fbd658d7a65c26fa21de3be7f9e894ec2ade5167
Benedict Golpeo <alanbenedictgolpeo@gmail.com>
2023-06-29 00:41:46 +0800

    lengthened prediction: https://app.clickup.com/t/865ckyhy4

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -114,7 +114,7 @@ class AnalyticsController extends Controller
 
         function monthly_sales($branch_product_id, $branch_id=4)
         {
-            $offset_month = 6;
+            $offset_month = 12;
             $tx = TransactionItem::whereHas('transaction', function($q) use($offset_month) {
                         $this_month = Carbon::now()->format('M-Y');
 
@@ -173,17 +173,41 @@ class AnalyticsController extends Controller
                 }
             }
 
+            # getting predicted next 2 months begin
+
+            $x = array_reverse($predictions);
+            $y = array_reverse($figures);
+
+            $predict_2nd = array_sum([
+                $y[0],
+                $y[1],
+                $x[0],
+            ]) / 3;
+
+            $predict_3rd = array_sum([
+                $y[0],
+                $x[0],
+                $predict_2nd,
+            ]) / 3;
+
+            array_push($predictions, $predict_2nd, $predict_3rd);
+
+            # getting predicted next 2 months end
+
             // PREDICTION APPENDS
-            $next_month = Carbon::parse(end($dates))->addMonths(1)->format('M Y');
-            $last_unpredicted_month = end($dates);
-            array_push($dates, $next_month);
+            $predict_first_month = Carbon::parse(end($dates))->addMonths(1)->format('M Y');
+            $predict_second_month = Carbon::parse(end($dates))->addMonths(2)->format('M Y');
+            $predict_third_month = Carbon::parse(end($dates))->addMonths(3)->format('M Y');
 
-            // $last_three = array_slice($figures, -3);
-            // $predicted_figure = array_sum($last_three) / 3;
-            // array_push($figures, $predicted_figure);
+            array_push($dates, $predict_first_month, $predict_second_month, $predict_third_month);
 
-            $dates = array_slice($dates, -4);
-            $figures = array_slice($figures, -3);
+            $last_three = array_slice($figures, -3);
+            $predicted_figure = array_sum($last_three) / 3;
+            array_push($figures, $predicted_figure);
+
+            $dates = array_slice($dates, -12);
+            $figures = array_slice($figures, -9);
+
 
             array_unshift($dates, 'Month');
             array_unshift($figures, 'Real Figures');
