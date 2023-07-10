461911793587f689222f536fec891c68bc6f8d8b
Benedict Golpeo <alanbenedictgolpeo@gmail.com>
2023-07-10 23:28:38 +0800

    initial

--- resources/views/printout/salesreport.blade.php
+++ resources/views/printout/salesreport.blade.php
@@ -24,28 +24,29 @@
                     </div>
                     <div style="display: flex;">
                         <span class="formField">
-                            <label for="monthYear1">Select Month and Year:</label>
-                            <input id="monthYear1" name="m_y" type="month" v-model="monthYear" style="height: 40px;">
+                            <label for="startDate1">Select starting date:</label>
+                            {{-- <input id="monthYear1" name="m_y" type="month" v-model="monthYear" style="height: 40px;"> --}}
+                            <input id="startDate1" name="start1" type="date" v-model="startDate1">
                         </span>
+                        <span class="formField">
+                            <label for="endDate1">Select end date:</label>
+                            {{-- <input id="monthYear1" name="m_y" type="month" v-model="monthYear" style="height: 40px;"> --}}
+                            <input id="endDate1" name="end1" type="date" v-model="endDate1">
+                        </span>
+                        @{{ startDate1 }}
                         <span class="formField">
                             <label>Select Branch:</label>
                             <input v-if="branch.id" name="branch_id" type="hidden" :value="branch.id">
-                            <multiselect
-                                v-model="branch"
-                                deselect-label="Can't remove this value"
-                                track-by="name"
-                                label="name"
-                                placeholder="Select Branch"
-                                :options="branches"
-                                :searchable="true"
-                                :allow-empty="false"
-                                style="min-width: 20vw;"
-                            />
+                            <multiselect v-model="branch" deselect-label="Can't remove this value" track-by="name"
+                                label="name" placeholder="Select Branch" :options="branches" :searchable="true"
+                                :allow-empty="false" style="min-width: 20vw;" />
                         </span>
                     </div>
 
                     <div>
-                        <button :disabled="monthYear && !branch.id" class="btn btn-sm btn-primary pull-left edit" type="submit" style="margin-top: 5px;">Generate PDF</button>
+                        <button :disabled="startDate1 && endDate1 && !branch.id"
+                            class="btn btn-sm btn-primary pull-left edit" type="submit" style="margin-top: 5px;">Generate
+                            PDF</button>
                     </div>
                 </div>
             </form>
@@ -61,9 +62,10 @@
             components: {
                 Multiselect: window.VueMultiselect.default,
             },
-            data () {
+            data() {
                 return {
-                    monthYear: '',
+                    startDate1: '',
+                    endDate1: '',
                     branch: [],
                     branches: {!! $branches ?? '' !!},
                 }
@@ -71,9 +73,22 @@
             methods: {
                 initialDate() {
                     var currentDate = new Date();
-                    var currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-                    var currentYear = currentDate.getFullYear().toString();
-                    this.monthYear = currentYear + '-' + currentMonth
+                    var year = currentDate.getFullYear();
+                    var month = ("0" + (currentDate.getMonth() + 1)).slice(-
+                        2); // Adding 1 to the month because it is zero-based
+
+                    // Get the starting day of the month
+                    var startingDay = "01";
+
+                    // Get the final day of the month
+                    var finalDay = new Date(year, currentDate.getMonth() + 1, 0).getDate();
+
+                    // Format the starting day and final day
+                    var formattedStartingDay = year + "-" + month + "-" + startingDay;
+                    var formattedFinalDay = year + "-" + month + "-" + finalDay;
+
+                    this.startDate1 = formattedStartingDay
+                    this.endDate1 = formattedFinalDay
                 },
 
             },
