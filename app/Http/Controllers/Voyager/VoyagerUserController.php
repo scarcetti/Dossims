1296603b936efd4fe16876ec1c18d6e5521bc93e
Benedict Golpeo <alanbenedictgolpeo@gmail.com>
2023-07-12 01:44:55 +0800

    auto adding branch employee

--- app/Http/Controllers/Voyager/VoyagerUserController.php
+++ app/Http/Controllers/Voyager/VoyagerUserController.php
@@ -1,14 +1,73 @@
 <?php
 
 namespace App\Http\Controllers\Voyager;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use TCG\Voyager\Events\BreadDataAdded;
 use TCG\Voyager\Facades\Voyager;
+use App\Models\BranchEmployee;
 
 use TCG\Voyager\Http\Controllers\VoyagerUserController as BaseVoyagerUserController;
 
 class VoyagerUserController extends BaseVoyagerUserController
 {
+    function branch() {
+        $user = Auth::user();
+        if(is_null($user)) {
+            return view('voyager::login');
+        }
+
+        $x = \App\Models\Branch::select('id')->whereHas('branchEmployees.employee.user', function($q) use ($user) {
+                                    $q->where('id', $user->id);
+                                })->first();
+        return is_null($x) ? false : $x->id;
+    }
+
+    function branchEmployee($request)
+    {
+        BranchEmployee::updateOrCreate(
+            [
+                'employee_id' => $request['employee_id'],
+            ],
+            [
+                'branch_id' => $this->branch(),
+            ]
+        );
+    }
+
+    public function store(Request $request)
+    {
+        $slug = $this->getSlug($request);
+
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Check permission
+        $this->authorize('add', app($dataType->model_name));
+
+        // Validate fields with ajax
+        $val = $this->validateBread($request->all(), $dataType->addRows)->validate();
+        $data = $this->insertUpdateData($request, $slug, $dataType->addRows, new $dataType->model_name());
+
+        event(new BreadDataAdded($dataType, $data));
+
+        $this->branchEmployee($request->all());
+        if (!$request->has('_tagging')) {
+            if (auth()->user()->can('browse', $data)) {
+                $redirect = redirect()->route("voyager.{$dataType->slug}.index");
+            } else {
+                $redirect = redirect()->back();
+            }
+
+            return $redirect->with([
+                'message'    => __('voyager::generic.successfully_added_new')." {$dataType->getTranslatedAttribute('display_name_singular')}",
+                'alert-type' => 'success',
+            ]);
+        } else {
+            return response()->json(['success' => true, 'data' => $data]);
+        }
+    }
+
     public function profile(Request $request)
     {
         $route = '';
@@ -32,6 +91,7 @@ class VoyagerUserController extends BaseVoyagerUserController
             ]);
         }
 
+        $this->branchEmployee($request->all());
         return parent::update($request, $id);
     }
 }
